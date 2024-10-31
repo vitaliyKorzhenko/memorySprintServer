9d1eac400d821d6d1b0b122ce0eaf341ea8456bf
Olga <888yalosinger@ukr.net>
2024-10-31 13:06:02 -0500

    add sql script file and start feedback method

---
+++ src/controllers/feetbackController.ts
@@ -0,0 +1,38 @@
+import { Request, Response } from 'express'; // Импортируем типы
+import pool from '../config/db';
+
+export const createFeedback = async (re: Request, res: Response): Promise<void> => {
+    try {
+        
+    }
+}
+
+export const deleteFeedback = async (re: Request, res: Response): Promise<void> => {
+    try {
+        
+    }
+}
+
+export const getAllFeedbacks = async (re: Request, res: Response): Promise<void> => {
+    try {
+        
+    }
+}
+
+export const getFeedbacksByUsers = async (re: Request, res: Response): Promise<void> => {
+    try {
+        
+    }
+}
+
+export const getFeedbacksByRating = async (re: Request, res: Response): Promise<void> => {
+    try {
+        
+    }
+}
+
+export const getFeedbacksByDate = async (re: Request, res: Response): Promise<void> => {
+    try {
+        
+    }
+}
